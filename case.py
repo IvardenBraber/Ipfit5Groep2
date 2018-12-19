b4b9175e09d3fd36cd1c6cfb996682408a357ed7
IvardenBraber <trolled@Live.nl>
2018-12-19 13:17:29 +0100

    bullshit verwijderd

--- case.py
+++ case.py
@@ -1,9 +1,10 @@
 import datetime
 class case:
-    def __init__(self, case_Name, serial_Number_Case, imaged_Number, serial_Number_Carrier, case_Summary):
+    def __init__(self, case_Name, case_Folder, case_Summary, serial_Number_Case, imaged_Number, serial_Number_Carrier):
         self.case_Name = case_Name
+        self.folder = case_Folder
+        self.summary = case_Summary
         self.date = datetime.datetime.now()
         self.serial_Number_Case = serial_Number_Case
         self.imaged_Number = imaged_Number
         self.serial_Number_Carrier = serial_Number_Carrier
-        self.summary = case_Summary
