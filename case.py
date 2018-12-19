163fc40ab689f2bfe8c65dd78d6865f7deba854a
IvardenBraber <trolled@Live.nl>
2018-12-19 13:06:02 +0100

    constructor aangemaakt voor het aanmaken van een case

--- case.py
+++ case.py
@@ -1,1 +1,9 @@
-
+import datetime
+class case:
+    def __init__(self, case_Name, serial_Number_Case, imaged_Number, serial_Number_Carrier, case_Summary):
+        self.case_Name = case_Name
+        self.date = datetime.datetime.now()
+        self.serial_Number_Case = serial_Number_Case
+        self.imaged_Number = imaged_Number
+        self.serial_Number_Carrier = serial_Number_Carrier
+        self.summary = case_Summary
