fa90c90e1896e9d80ac617467d2608ccb71bcc47
IvardenBraber <trolled@Live.nl>
2019-01-09 14:59:57 +0100

    case bestand aangepast

--- case.py
+++ case.py
@@ -5,9 +5,7 @@ class case:
         self.folder = case_folder
         self.summary = case_summary
         self.date = datetime.datetime.now()
-        self.serial_number_case = serial_number_case
-        self.imaged_number = imaged_number
-        self.serial_number_carrier = serial_number_carrier
+
 
     def get_case_name(self):
         return self.case_name
@@ -15,14 +13,8 @@ class case:
         return self.case_folder
     def get_case_summary(self):
         return self.case_summary
-    def get_serial_number_case(self):
-        return self.serial_number_case
-    def get_imaged_number(self):
-        return self.imaged_number
     def get_date(self):
         return self.date
-    def get_serial_number_carrier(self):
-        return self.serial_number_carrier
 
     def set_case_name(self, new_value):
         self.case_name = new_value
@@ -30,11 +22,5 @@ class case:
         self.case_folder = new_value
     def set_case_summary(self, new_value):
         self.case_summary = new_value
-    def set_serial_number_case(self, new_value):
-        self.serial_number_case = new_value
-    def set_imaged_number(self, new_value):
-        self.imaged_number = new_value
     def set_date(self):
         self.date = datetime.datetime.now()
-    def set_serial_number_carrier(self, new_value):
-        self.serial_number_carrier = new_value
