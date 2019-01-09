f26265def5605d0e4c3661a050ca15d35c8cddf1
CalvinDijk <45353826+CalvinDijk@users.noreply.github.com>
2019-01-09 12:47:58 +0100

    Update op script met image inladen :100:

--- image.py
+++ image.py
@@ -5,6 +5,7 @@ import datetime
 import pyewf
 
 
+
 class ewf_Img_Info(pytsk3.Img_Info):
     def __init__(self, ewf_handle):
         self._ewf_handle = ewf_handle
@@ -22,44 +23,23 @@ class ewf_Img_Info(pytsk3.Img_Info):
         return self._ewf_handle.get_media_size()
 
 
-file_names = pyewf.glob("image_sd_pi.E01")
-
 ewf_handle = pyewf.handle()
 
-ewf_handle.open(file_names)
+filenames = pyewf.glob("image_sd_pi.E01")
+ewf_handle.open(filenames)
 
-image_handle = ewf_Img_Info(ewf_handle)
+imagehandle = ewf_Img_Info(ewf_handle)
 
-'''partitionTable = pytsk3.Volume_Info(image_handle)
+partitionTable = pytsk3.Volume_Info(imagehandle)
+
 for partition in partitionTable:
     print(partition.addr, partition.desc, "%ss(%s)" % (partition.start, partition.start * 512), partition.len)
-    if 'FAT32' in partition.desc:
-        filesystemObject = pytsk3.FS_Info(image_handle, offset=(partition.start * 512))
-        fileobject = filesystemObject.open("/$FAT1")
-        print("File Inode:", fileobject.info.meta.addr)
-        print("File Name:", fileobject.info.name.name)
-        print("File Creation Time:", datetime.datetime.fromtimestamp(fileobject.info.meta.crtime).strftime(
-            '%Y-%m-%d %H:%M:%S'))
-        outFileName = str(partition.addr) + fileobject.info.name.name
-        print(outFileName)
-        outfile = open(outFileName, 'w')
-        file_data = fileobject.read_random(0, fileobject.info.meta.size)
-        outfile.write(file_data)
-        outfile.close
 
-'''
-
-filesystemObject = pytsk3.FS_Info(image_handle, offset=4194304)
-fileobject = filesystemObject.open("/overlays")
+filesystemObject = pytsk3.FS_Info(imagehandle, offset= partition.start)
+fileobject = filesystemObject.open("/$FAT1")
 print("File Inode:",fileobject.info.meta.addr)
 print("File Name:",fileobject.info.name.name)
 print("File Creation Time:",datetime.datetime.fromtimestamp(fileobject.info.meta.crtime).strftime('%Y-%m-%d %H:%M:%S'))
 outfile = open('DFIRWizard-output', 'w')
-file_data = fileobject.read_random(0,fileobject.info.meta.size)
-outfile.write(file_data)
-
-imagefile = "Stage2.vhd"
-imagehandle = pytsk3.Img_Info(imagefile)
-partitionTable = pytsk3.Volume_Info(imagehandle)
-for partition in partitionTable:
-  print(partition.addr, partition.desc, "%ss(%s)" % (partition.start, partition.start * 512), partition.len)
+filedata = fileobject.read_random(0,fileobject.info.meta.size)
+outfile.write(filedata)
